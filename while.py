1adfe9463e0da335e1e83f405c5812e2efbf3343
CoderAvazbek <programmingavazbek@gmail.com>
2024-07-08 22:05:00 +0500

    ..

---
+++ while.py
@@ -0,0 +1,10 @@
+# shart = True
+# dustlar = []
+#
+# while shart:
+#     dust = input("yaqin dustingizni ismini kiriting:")
+#     dustlar.append(dust)
+#     savol = input("yana dust qushaszmi:(ha/yuq) ").lower()
+#     if savol == "yuq":
+#         shart = False
+# print(dustlar)
